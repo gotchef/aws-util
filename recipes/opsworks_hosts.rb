ad139db291c6703289bac27cced9752c8639b790
Franklin Wise <franklin@krave-n.com>
2015-02-09 18:29:14 -0800

    null check

--- recipes/opsworks_hosts.rb
+++ recipes/opsworks_hosts.rb
@@ -1,17 +1,21 @@
 # aws-util::opsworks_hosts
 # extracts the opsworks layer ip and hosts into an aws agnostic structure
-
-layers = node[:opsworks][:layers]
-layers.each do | layername, layer|
-	instances = layer[:instances]
-	ips = []
-	hosts = []
-	instances.each do |hostname, instance| 
-		hosts.push(hostname)
-		ips.push(instance[:private_ip])
+if node[:opsworks].nil? 
+	Chef::Log.info('not running inside opsworks, can not pick up opsworks hosts')	
+else
+	layers = node[:opsworks][:layers]
+	layers.each do | layername, layer|
+		instances = layer[:instances]
+		ips = []
+		hosts = []
+		instances.each do |hostname, instance| 
+			hosts.push(hostname)
+			ips.push(instance[:private_ip])
+		end
+	
+		node.default[:stack][layername][:ips] = ips
+		node.default[:stack][layername][:hosts] = hosts
 	end
-
-	node.default[:stack][layername][:ips] = ips
-	node.default[:stack][layername][:hosts] = hosts
+	
+	node.default[:me][layers] = node[:opsworks][:instance][:layers].map(&:downcase)
 end
-
